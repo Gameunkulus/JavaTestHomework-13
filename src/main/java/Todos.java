d1b21ab0d210681a6afc5f1c2c645868fdb86949
Gameunkulus <bugr24-8@mail.ru>
2023-06-24 10:23:25 +0300

    program redacted

---
+++ src/main/java/Todos.java
@@ -0,0 +1,25 @@
+public class Todos {
+
+    private Task[] tasks = new Task[0];
+
+    private Task[] addToArray(Task[] current, Task task) {
+        Task[] tmp = new Task[current.length + 1];
+        for (int i = 0; i < current.length; i++) {
+            tmp[i] = current[i];
+        }
+        tmp[tmp.length - 1] = task;
+        return tmp;
+    }
+
+    public void add(Task task) {
+        tasks = addToArray(tasks, task);
+    }
+
+    public boolean matches(String query){
+        return false;
+    }
+
+    public Task[] findAll() {
+        return tasks;
+    }
+}
---
+++ src/test/java/TodosTest.java
@@ -0,0 +1,57 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class TodosTest {
+    @Test
+    public void shouldAddThreeTasksOfDifferentType() {
+        SimpleTask simpleTask = new SimpleTask(5, "Позвонить родителям");
+
+        String[] subtasks = {"Молоко", "Яйца", "Хлеб"};
+        Epic epic = new Epic(55, subtasks);
+
+        Meeting meeting = new Meeting(
+                555,
+                "Выкатка 3й версии приложения",
+                "Приложение НетоБанка",
+                "Во вторник после обеда"
+        );
+
+        Todos todos = new Todos();
+
+        todos.add(simpleTask);
+        todos.add(epic);
+        todos.add(meeting);
+
+        Task[] expected = {simpleTask, epic, meeting};
+        Task[] actual = todos.findAll();
+        Assertions.assertArrayEquals(expected, actual);
+
+    }
+
+    @Test
+    void matchesTest() {
+        SimpleTask simpleTask = new SimpleTask(5, "Позвонить родителям");
+
+        String[] subtasks = {"Молоко", "Яйца", "Хлеб"};
+        Epic epic = new Epic(55, subtasks);
+
+        Meeting meeting = new Meeting(
+                555,
+                "Выкатка 3й версии приложения",
+                "Приложение НетоБанка",
+                "Во вторник после обеда"
+        );
+
+        Todos todos = new Todos();
+
+        todos.add(simpleTask);
+        todos.add(epic);
+        todos.add(meeting);
+
+        Assertions.assertTrue(epic.matches("Молоко"));
+        Assertions.assertTrue(meeting.matches("Выкатка 3й версии приложения"));
+        Assertions.assertTrue(meeting.matches("Приложение НетоБанка"));
+        Assertions.assertTrue(simpleTask.matches("Позвонить родителям"));
+
+    }
+}
